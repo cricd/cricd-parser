8c3d4f86785103ccbd96d36bc6f11e25f000eaee
Ryan Scott <ryankennethscott@gmail.com>
2016-05-29 17:21:05 +1200

    Changes

--- score_endpoint/score_endpoint.rb
+++ score_endpoint/score_endpoint.rb
@@ -1,56 +1,117 @@
 require 'sinatra'
 require 'json'
-#require_relative 'score_consumer.rb'
+require 'date'
+require_relative '../cricket_event.rb'
+require_relative '../cricket_score.rb'
+require_relative '../cricket_event_store.rb'
 
 
-def coerce(param, type)
-  begin
-    return nil if param.nil?
-    return param if (param.is_a?(type) rescue false)
-    return Integer(param) if type == Integer
-    return Float(param) if type == Float
-    return String(param) if type == String
-    return Date.parse(param) if type == Date
-    return Time.parse(param) if type == Time
-    return DateTime.parse(param) if type == DateTime
-    return nil
-  rescue ArgumentError
-     puts "'#{param}' is not a valid #{type}"
-  end
+
+module CricketScoreConsumer
+# TODO: Implement match searching
+  def self.get_score_at_over_ball(match_id, innings, over, ball)
+  #Score for innings are going to be in a hash e.g. "1" => Innings_score
+  score = {}
+  # Read all the events from the stream
+   events = CricketEventStore.read_all_events()
+   events.each do |event|
+    cricket_event = CricketEvent.new(event) 
+    # Stop counting if we've reached our limit
+    break if ((cricket_event.innings.to_i > innings.to_i) or\
+              ((cricket_event.innings.to_i == innings.to_i) and (cricket_event.over.to_i > over.to_i)) or \
+              ((cricket_event.innings.to_i == innings.to_i) and (cricket_event.over.to_i == over.to_i) and cricket_event.ball.to_i > ball.to_i))
+    current_innings = event["data"]["ball"]["innings"]
+     # If we've got some data on this innings
+     if score.has_key?(current_innings)
+       score[current_innings].update_from_event(cricket_event)
+     else
+       score[current_innings] = CricketScore.new(
+         event["data"]["runs"].to_i,
+         event["data"]["ball"]["over"].to_i,
+         event["data"]["ball"]["ball"].to_i,
+         cricket_event.is_wicket?() ? 1 : 0)
+     end
+   end
+   score.values.map! {|x| x.to_s}
+   return score.to_json
 end
 
-def check_required_parameters(params, possible_params)
-  # Check all the parameters
-  required_params_exist = required_params.map { |x| params.include?(x) ? true : x}
-  unless required_params_exist.all? { |param| param == true }
-    puts "Missing #{required_params_exist} parameter(s)"
-    return false
-  end
-  return true
+  def self.get_score_at_datetime(match_id, datetime)
+  #Score for innings are going to be in a hash e.g. "1" => Innings_score
+    score = {}
+    end_datetime = DateTime.iso8601(datetime)
+  # Read all the events from the stream
+  events = CricketEventStore.read_all_events()
+  events.each do |event|
+    cricket_event = CricketEvent.new(event) 
+    # Stop counting if we've reached our limit
+    current_datetime = DateTime.parse(cricket_event.timestamp)
+    break if (current_datetime >= end_datetime)
+    current_innings = event["data"]["ball"]["innings"]
+     # If we've got some data on this innings
+     if score.has_key?(current_innings)
+       score[current_innings].update_from_event(cricket_event)
+     else
+       score[current_innings] = CricketScore.new(
+         event["data"]["runs"].to_i,
+         event["data"]["ball"]["over"].to_i,
+         event["data"]["ball"]["ball"].to_i,
+         cricket_event.is_wicket?() ? 1 : 0)
+     end
+   end
+   score.values.map! {|x| x.to_s}
+   return score.to_json
+end
 end
 
-def check_parameter_types(params, possible_params)
-  correct_param_values = params.map {|key, value| coerce(value, possible_params[key][:type]) ? true : key}
-  unless correct_param_values.all? { |param| param == true }
-    puts "Parameter#{correct_param_values.delete_if{ |x| x==true }} is not the right type"
-    return false
-  end
-  return true
-end
+# def coerce(param, type)
+#   begin
+#     return nil if param.nil?
+#     return param if (param.is_a?(type) rescue false)
+#     return Integer(param) if type == Integer
+#     return Float(param) if type == Float
+#     return String(param) if type == String
+#     return Date.parse(param) if type == Date
+#     return Time.parse(param) if type == Time
+#     return DateTime.parse(param) if type == DateTime
+#     return nil
+#   rescue ArgumentError
+#      puts "'#{param}' is not a valid #{type}"
+#   end
+# end
 
-def check_parameter_values(params, possible_params)
-  # Check all the min/max
-  min_values = possible_params.map { |key, value| value[:min] ? key : nil }.compact
-  max_values = possible_params.map { |key, value| value[:max] ? key : nil }.compact
+# def check_required_parameters(params, possible_params)
+#   # Check all the parameters
+#   required_params_exist = required_params.map { |x| params.include?(x) ? true : x}
+#   unless required_params_exist.all? { |param| param == true }
+#     puts "Missing #{required_params_exist} parameter(s)"
+#     return false
+#   end
+#   return true
+# end
 
-  min_values_satisfied = min_values.map { |key| params[key] > possible_params[key][:min]}.all?
-  max_values_satisfied = max_values.map { |key| params[key] > possible_params[key][:max]}.all?
-  unless (max_values_satisfied and min_values_satisfied)
-    puts "Not all maximum values satisfied"
-    return false
-  end
-  return true
-end
+# def check_parameter_types(params, possible_params)
+#   correct_param_values = params.map {|key, value| coerce(value, possible_params[key][:type]) ? true : key}
+#   unless correct_param_values.all? { |param| param == true }
+#     puts "Parameter#{correct_param_values.delete_if{ |x| x==true }} is not the right type"
+#     return false
+#   end
+#   return true
+# end
+
+# def check_parameter_values(params, possible_params)
+#   # Check all the min/max
+#   min_values = possible_params.map { |key, value| value[:min] ? key : nil }.compact
+#   max_values = possible_params.map { |key, value| value[:max] ? key : nil }.compact
+
+#   min_values_satisfied = min_values.map { |key| params[key] > possible_params[key][:min]}.all?
+#   max_values_satisfied = max_values.map { |key| params[key] > possible_params[key][:max]}.all?
+#   unless (max_values_satisfied and min_values_satisfied)
+#     puts "Not all maximum values satisfied"
+#     return false
+#   end
+#   return true
+# end
 
 # RESTful service that allows you to find the score
 # Requires:
@@ -73,13 +134,7 @@ get '/score' do
     "ball" => {:type => Integer, :required => false, :min => 0, :max => 5}
   }
 
-  required_params = check_required_parameters(params, possible_params)
-  correct_param_types = check_parameter_types(params, possible_params)
-  correct_param_values = check_parameter_values(params, possible_params)
 
-  puts required_params
-  puts correct_param_types
-  puts correct_param_values
   # if (required_params and correct_param_types and correct_param_values)
   #   if params.key?("over")
   #    result = CricketScoreConsumer.get_score_at_over_ball(params["match_id"], params["innings"], params["over"], params["ball"]) 
